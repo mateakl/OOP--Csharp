9e71dca0d5db6601bda40e903e42771540d8f1bf
mateakl <matea.klobucar1@gmail.com>
2025-12-08 17:15:02 +0100

    Initial commit

---
+++ Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AV1_vjezba
+{
+    public class Dog
+    {
+        public string name = "Cirko";
+        public string breed = "Doberman" ;
+        public string color = "Black";
+        public string identificator = "HR86928DC";
+
+
+        public string Growl()
+        {
+            return $"Grrrr!";
+        }
+        public string Bark()
+        {
+            return Growl() + $"Woof! Woof! I am {name} - {breed} - {color} - {identificator}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AV1_vjezba
+{
+    public class Program
+    {
+        public static void RunSimpleDemoDog()
+        {
+            Dog dog = new Dog();
+            dog.Growl();
+            dog.Bark();
+        }
+        static void Main(string[] args)
+        {
+            RunSimpleDemoDog();
+        }
+    }
+}
